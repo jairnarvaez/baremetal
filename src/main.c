d79c68200ea48ce905cbb16fb61a691bccdc6a7d
jairnarvaez <jairandresnar@gmail.com>
2026-01-14 16:30:21 -0500

    main: update animations

--- src/main.c
+++ src/main.c
@@ -5,16 +5,18 @@ int main(void)
     GPIO0.DIR = LED_MASK0;
     GPIO1.DIR = LED_MASK1;
 
-    uint32_t tiempo = 100;
-    uint32_t repetitions = 10;
+    uint32_t tiempo = 150;
+    uint32_t repetitions = 5;
 
     // Implementacion de la matriz de leds
-    animation_star(tiempo, repetitions);
-    animation_x_rotate(tiempo, repetitions);
-    animation_heart(tiempo, repetitions);
-    animation_arrow(2 * tiempo, repetitions);
-    animation_smile(tiempo, repetitions);
-    animation_pacman(tiempo, repetitions);
+    for (;;) {
+        animation_star(tiempo, repetitions);
+        animation_x_rotate(tiempo, repetitions);
+        animation_heart(tiempo, repetitions);
+        animation_arrow(2 * tiempo, repetitions);
+        animation_smile(tiempo, repetitions);
+        animation_pacman(tiempo, repetitions);
+    }
 
     return 0;
 }
