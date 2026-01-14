5fd585cff90b567a8cad27aa8e6fd59fe93979cb
jairnarvaez <jairandresnar@gmail.com>
2026-01-14 00:10:36 -0500

    driver: add new macros

--- drivers/inc/led_microbit.h
+++ drivers/inc/led_microbit.h
@@ -10,6 +10,12 @@
 #define ROW4 24
 #define ROW5 19
 
+#define COL1 28
+#define COL2 11
+#define COL3 31
+#define COL4 5
+#define COL5 30
+
 struct _gpio {
     char _pad1[4]; // 0x500
     unsigned OUT; // 0x504
@@ -20,6 +26,16 @@ struct _gpio {
     unsigned DIRSET; // 0x518
     unsigned DIRCLR; // 0x51C
 };
+
+typedef enum {
+    LED_OFF = 0,
+    LED_ON = 1
+} LedState;
+
+#define _SET(p) (1 << p)
+
+#define _ROW_MASK _SET(ROW1) | _SET(ROW2) | _SET(ROW3) | _SET(ROW4) | _SET(ROW5)
+#define _COL_MASK _SET(COL1) | _SET(COL2) | _SET(COL3) | _SET(COL5)
 
 #define _ROW(row, c1, c2, c3, c4, c5) \
     _ROW_GPIO0(row, c1, c2, c3, c5), _ROW_GPIO1(c4)
