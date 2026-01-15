5333dfe76d6537b3e80c4b850e08f40c650ec0af
jairnarvaez <jairandresnar@gmail.com>
2026-01-15 12:37:36 -0500

    driver/led: remove unused functions

--- drivers/inc/led.h
+++ drivers/inc/led.h
@@ -3,14 +3,8 @@
 #include "delay.h"
 #include "gpio.h"
 
-void blink_main();
-
-void led_set(int row, int col, LedState state);
-void led_blink(int row, int col);
-void led_blink_test();
 void error_blink();
-
-void set_row(int row, LedState state);
-void set_col(int col, LedState state);
-
+void set_led(int row, uint8_t pattern);
 void show(const uint8_t* img, int time);
+
+void blink_main();
