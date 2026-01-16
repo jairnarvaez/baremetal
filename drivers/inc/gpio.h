71435aa7db18a882982b4ebcbe577b483a88e558
jairnarvaez <jairandresnar@gmail.com>
2026-01-15 19:28:52 -0500

    driver/gpio: add some PIN_CNF macros and refactor BASE macro

--- drivers/inc/gpio.h
+++ drivers/inc/gpio.h
@@ -1,8 +1,11 @@
+#define BASE0 0x50000500
+#define BASE1 0x50000800
+
 #define LED_MASK0 0xd1688800
 #define LED_MASK1 0x00000020
 
-#define GPIO0 (*(volatile struct _gpio*)0x50000500)
-#define GPIO1 (*(volatile struct _gpio*)0x50000800)
+#define GPIO0 (*(volatile struct _gpio*)BASE0)
+#define GPIO1 (*(volatile struct _gpio*)BASE1)
 
 #define ROW1 21
 #define ROW2 22
@@ -25,6 +28,8 @@ struct _gpio {
     unsigned DIR; // 0x514
     unsigned DIRSET; // 0x518
     unsigned DIRCLR; // 0x51C
+    char _pad3[480]; // 0x520
+    unsigned PIN_CNF[32]; // 0x700
 };
 
 typedef enum {
@@ -46,3 +51,17 @@ typedef enum {
 
 #define _ROW_GPIO1(c4) \
     ((!c4) << 5)
+
+#define PIN_CNF_DIR 0
+#define PIN_CNF_INPUT 1
+#define PIN_CNF_PULL 2
+#define PIN_CNF_DRIVE 8
+#define PIN_CNF_SENSE 16
+
+#define CNF_DIR_INPUT 0
+#define CNF_DIR_OUTPUT 1
+#define CNF_INPUT_CONNECT 0
+#define CNF_INPUT_DISCONNECT 1
+#define CNF_PULL_DISABLED 0
+#define CNF_PULL_DOWN 1
+#define CNF_PULL_UP 3
