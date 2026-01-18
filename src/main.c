8e2325626fe502a2568e673dc628407cd915972b
jairnarvaez <jairandresnar@gmail.com>
2026-01-18 15:56:20 -0500

    src/main: add heart animation example for led matrix

--- src/main.c
+++ src/main.c
@@ -1,31 +1,25 @@
+#include "animations.h"
 #include "nvic.h"
 #include "timer.h"
 #include "uart.h"
 #include "utils.h"
 
-int cont = 0;
-char cont_string[100];
-
-void timer0_irqhandler(void)
-{
-    int2string(cont, cont_string);
-    uart_send("Timer activado: ", cont_string, "\r\n");
-    cont++;
-
-    if (TIMER0.EVENTS_COMPARE[0]) {
-        TIMER0.EVENTS_COMPARE[0] = 0;
-    }
-}
-
 int main(void)
 {
     uart_init();
+    display_init(REFRESH_RATE_HZ);
     uart_send("\n===========================\r\n");
     uart_send("     UART INICIADO             \r\n");
     uart_send("===========================\r\n");
-    timer_init();
 
     for (;;) {
+        uart_send("\nAnimacion 1\r\n");
+        display(HEART_BIG);
+        delay(700000);
+
+        uart_send("Animacion 2\r\n");
+        display(HEART_SMALL);
+        delay(250000);
     }
     return 0;
 }
