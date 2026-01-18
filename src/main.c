7204343f9b7f48d6dd426ef201250d4e4ba4a92a
jairnarvaez <jairandresnar@gmail.com>
2026-01-17 22:46:25 -0500

    src/main: integrate TIMER0 and UART for interrupt testing

--- src/main.c
+++ src/main.c
@@ -1,11 +1,31 @@
-#include "animations.h"
-#include "delay.h"
+#include "nvic.h"
+#include "timer.h"
 #include "uart.h"
+#include "utils.h"
+
+int cont = 0;
+char cont_string[100];
+
+void timer0_irqhandler(void)
+{
+    int2string(cont, cont_string);
+    uart_send("Timer activado: ", cont_string, "\r\n");
+    cont++;
+
+    if (TIMER0.EVENTS_COMPARE[0]) {
+        TIMER0.EVENTS_COMPARE[0] = 0;
+    }
+}
 
 int main(void)
 {
     uart_init();
-    uart_send("Hola mundo :)\r\n");
-    animations_main();
+    uart_send("\n===========================\r\n");
+    uart_send("     UART INICIADO             \r\n");
+    uart_send("===========================\r\n");
+    timer_init();
+
+    for (;;) {
+    }
     return 0;
 }
