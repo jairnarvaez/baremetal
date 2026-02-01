61fae0dc633a9b4fc5512483d9313936fdef808c
jairnarvaez <jairandresnar@gmail.com>
2026-02-01 16:43:40 -0500

    inc/buttons: add GPIOe interrupt support for buttons

--- drivers/inc/buttons.h
+++ drivers/inc/buttons.h
@@ -2,6 +2,8 @@
 #define BUTTONS_H
 
 #include "gpio.h"
+#include "gpioe.h"
+#include "nvic.h"
 
 #define BUTTON_A_PIN 14
 #define BUTTON_B_PIN 23
@@ -13,6 +15,18 @@ static inline void buttons_init(void)
 {
     GPIO_Input(&GPIO0, BUTTON_A_PIN, GPIO_PULL_UP);
     GPIO_Input(&GPIO0, BUTTON_B_PIN, GPIO_PULL_UP);
+}
+
+static inline void buttons_init_gpioe(GPIOE_Channel_t channel_Button_A, GPIOE_Channel_t channel_Button_B)
+{
+    GPIO_Input(&GPIO0, BUTTON_A_PIN, GPIO_PULL_UP);
+    GPIO_Input(&GPIO0, BUTTON_B_PIN, GPIO_PULL_UP);
+
+    GPIOTE_ConfigEvent(&GPIOE, channel_Button_A, BUTTON_A_PIN, GPIOE_PORT_0, GPIOE_POLARITY_TOGGLE);
+    GPIOTE_ConfigEvent(&GPIOE, channel_Button_B, BUTTON_B_PIN, GPIOE_PORT_0, GPIOE_POLARITY_TOGGLE);
+
+    GPIOE.INTENSET = (1 << 0) | (1 << 1) | (1 << 31);
+    NVIC_EnableIRQ(6);
 }
 
 #endif
