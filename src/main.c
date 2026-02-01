4b02efc78994658ce67819860c0612e4be28a17f
jairnarvaez <jairandresnar@gmail.com>
2026-01-31 23:38:01 -0500

    src/main: update main function for button test

--- src/main.c
+++ src/main.c
@@ -1,27 +1,33 @@
+#include "animations.h"
+#include "buttons.h"
+#include "display.h"
 #include "uart.h"
-
-#define ECHO_BUFFER_SIZE 64
-
-char echo_buffer[ECHO_BUFFER_SIZE] = { 0 };
-
-void on_receive_msg(size_t bytes)
-{
-    uart_send(echo_buffer);
-}
 
 int main(void)
 {
     uart_init(UART_BAUDRATE_115200);
+    buttons_init();
+    display_init(60);
 
     uart_send("\r\n");
-    uart_send("===========================\r\n");
-    uart_send("   UART ECHO MODE ACTIVE   \r\n");
-    uart_send("===========================\r\n");
-    uart_send("Waiting for data...\r\n\r\n");
+    uart_send("================================\r\n");
+    uart_send("  micro:bit v2 - Demo Program  \r\n");
+    uart_send("================================\r\n");
+    uart_send("Button A: Heart (big)\r\n");
+    uart_send("Button B: Heart (small)\r\n");
+    uart_send("No button: Smile\r\n");
+    uart_send("Ready!\r\n\r\n");
+
+    display(SMILE);
 
     while (1) {
-        uart_receive(echo_buffer, 1);
-        echo_buffer[0] = '\0';
+        if (button_a_is_pressed()) {
+            display(HEART_BIG);
+        } else if (button_b_is_pressed()) {
+            display(HEART_SMALL);
+        } else {
+            display(SMILE);
+        }
     }
 
     return 0;
