38af618c87c5193596a05a95315f643899632acc
jairnarvaez <jairandresnar@gmail.com>
2026-01-31 21:16:04 -0500

    driver/display: refactor library

--- drivers/inc/display.h
+++ drivers/inc/display.h
@@ -1,12 +1,13 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "gpio.h"
 #include <stdint.h>
 
-#include "delay.h"
-#include "gpio.h"
+#define DISPLAY_ROWS 5
+#define DISPLAY_COLS 5
 
-#define REFRESH_RATE_HZ 60
-
-#define LED_MASK0 0xd1688800
-#define LED_MASK1 0x00000020
+#define DISPLAY_DEFAULT_FPS 60
 
 typedef enum {
     LED_ROW1 = 21,
@@ -14,7 +15,6 @@ typedef enum {
     LED_ROW3 = 15,
     LED_ROW4 = 24,
     LED_ROW5 = 19,
-
     LED_COL1 = 28,
     LED_COL2 = 11,
     LED_COL3 = 31,
@@ -22,13 +22,11 @@ typedef enum {
     LED_COL5 = 30,
 } GPIO_LedMatrix_t;
 
-void error_blink();
-void display_row(int row, uint8_t pattern);
+void display_init(uint32_t refresh_rate_hz);
 void display(const uint8_t* row);
-void display_clear();
-void display_init(uint32_t display_fps);
+void display_clear(void);
 void display_set_pixel(uint8_t x, uint8_t y, uint8_t state);
-void display_shift_right();
-void display_shift_left();
+void display_shift_right(void);
+void display_shift_left(void);
 
-void timer0_irqhandler(void);
+#endif
--- drivers/src/display.c
+++ drivers/src/display.c
@@ -1,45 +1,19 @@
+#include <stdint.h>
+
 #include "display.h"
+#include "error.h"
 #include "gpio.h"
 #include "timer.h"
-#include "uart.h"
 #include "utils.h"
-#include <stdint.h>
+
+#define LED_MASK0 0xd1688800
+#define LED_MASK1 0x00000020
 
 const GPIO_LedMatrix_t ROWS[] = { LED_ROW1, LED_ROW2, LED_ROW3, LED_ROW4, LED_ROW5 };
 const GPIO_LedMatrix_t COLS[] = { LED_COL1, LED_COL2, LED_COL3, LED_COL4, LED_COL5 };
 
 uint8_t currentRow = 0;
 uint8_t buffer_row_value[5];
-
-uint8_t ERROR[] = {
-    0b11111,
-    0b10000,
-    0b11100,
-    0b10000,
-    0b11111
-};
-
-char numero[100];
-
-void timer0_irqhandler(void)
-{
-    display_row(currentRow, 0b00000);
-    currentRow = (currentRow + 1) % 5;
-    display_row(currentRow, buffer_row_value[currentRow]);
-
-    if (TIMER0.EVENTS_COMPARE[0]) {
-        TIMER0.EVENTS_COMPARE[0] = 0;
-    }
-}
-
-void error_blink()
-{
-    GPIO0.DIR = LED_MASK0;
-    GPIO1.DIR = LED_MASK1;
-    while (1) {
-        display(ERROR);
-    }
-}
 
 void display_row(int row, uint8_t pattern)
 {
@@ -90,9 +64,17 @@ void display_clear()
 
 void display_init(uint32_t refresh_rate_hz)
 {
-    timer_init(1000000 / (refresh_rate_hz * 5));
-    GPIO0.DIR = LED_MASK0;
-    GPIO1.DIR = LED_MASK1;
+    uint32_t pin_config = GPIO_PIN_CNF_PACK(
+        GPIO_DIR_OUTPUT,
+        GPIO_INPUT_DISCONNECT,
+        GPIO_PULL_DISABLED,
+        GPIO_DRIVE_S0S1,
+        GPIO_SENSE_DISABLED);
+
+    GPIO_Config_Mask(&GPIO0, MATRIX_ROW_MASK | MATRIX_COL_PORT0_MASK, pin_config);
+    GPIO_Config_Mask(&GPIO1, MATRIX_COL_PORT1_MASK, pin_config);
+
+    timer_init(1000000 / (refresh_rate_hz * DISPLAY_ROWS));
 }
 
 void display_set_pixel(uint8_t x, uint8_t y, uint8_t state)
@@ -119,5 +101,16 @@ void display_shift_left(void)
     for (int y = 0; y < 5; y++) {
         buffer_row_value[y] <<= 1;
         buffer_row_value[y] &= 0x1F;
+    }
+}
+
+void timer0_irqhandler(void)
+{
+    display_row(currentRow, 0b00000);
+    currentRow = (currentRow + 1) % 5;
+    display_row(currentRow, buffer_row_value[currentRow]);
+
+    if (TIMER0.EVENTS_COMPARE[0]) {
+        TIMER0.EVENTS_COMPARE[0] = 0;
     }
 }
