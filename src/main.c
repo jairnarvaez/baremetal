4df8a554255faa980774b21c23812b0bcd8894da
jairnarvaez <jairandresnar@gmail.com>
2026-02-01 23:53:04 -0500

    src/main: add compile-time test selector

--- src/main.c
+++ src/main.c
@@ -1,63 +1,31 @@
-#include "animations.h"
-#include "buttons.h"
-#include "display.h"
-#include "uart.h"
+#include "test_buttons.h"
+#include "test_delay.h"
+#include "test_uart.h"
 
-typedef enum {
-    STATE_IDLE,
-    STATE_BUTTON_A,
-    STATE_BUTTON_B
-} app_state_t;
+#define SELECTED_TEST 2
 
-static volatile app_state_t current_state = STATE_IDLE;
+#define TEST_UART 1
+#define TEST_BUTTONS 2
+#define TEST_DELAY 3
 
 int main(void)
 {
-    uart_init(UART_BAUDRATE_115200);
-    buttons_init_gpiote(GPIOET_CHANNEL_0, GPIOET_CHANNEL_1);
-    display_init(60);
+#if SELECTED_TEST == TEST_UART
+    test_uart();
 
-    uart_send("\r\n");
-    uart_send("================================\r\n");
-    uart_send("  micro:bit v2 - Demo Program  \r\n");
-    uart_send("================================\r\n");
-    uart_send("Button A: Heart (big)\r\n");
-    uart_send("Button B: Heart (small)\r\n");
-    uart_send("Ready!\r\n\r\n");
+#elif SELECTED_TEST == TEST_BUTTONS
+    test_buttons();
+
+#elif SELECTED_TEST == TEST_DELAY
+    uint32_t time_us = 1000000;
+    test_delay(time_us);
+
+#else
+#error "No test selected or invalid test number"
+#endif
 
     for (;;) {
-
-        switch (current_state) {
-
-        case STATE_IDLE:
-            break;
-
-        case STATE_BUTTON_A:
-            display(HEART_BIG);
-            break;
-
-        case STATE_BUTTON_B:
-            display(HEART_SMALL);
-            break;
-
-        default:
-            break;
-        }
     }
-}
 
-void GPIOTE_Channel0_Callback(void)
-{
-    if (button_a_is_pressed()) {
-        uart_send("Button A pressed\r\n");
-        current_state = STATE_BUTTON_A;
-    }
-}
-
-void GPIOTE_Channel1_Callback(void)
-{
-    if (button_b_is_pressed()) {
-        uart_send("Button B pressed\r\n");
-        current_state = STATE_BUTTON_B;
-    }
+    return 0;
 }
