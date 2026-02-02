59f5391604ad309d0fa533f8fc3fec5f1c11fdc2
jairnarvaez <jairandresnar@gmail.com>
2026-02-01 23:49:38 -0500

    tests/uart: add test uart function

---
+++ tests/test_uart.c
@@ -0,0 +1,29 @@
+#include "uart.h"
+
+#define ECHO_BUFFER_SIZE 64
+
+char echo_buffer[ECHO_BUFFER_SIZE] = { 0 };
+
+void on_receive_msg(size_t bytes)
+{
+    uart_send(echo_buffer);
+}
+
+int test_uart(void)
+{
+    uart_init(UART_BAUDRATE_115200);
+
+    uart_send("\r\n");
+    uart_send("================================\r\n");
+    uart_send("   micro:bit v2 - UART Test    \r\n");
+    uart_send("================================\r\n");
+
+    uart_send("Waiting for data...\r\n\r\n");
+
+    while (1) {
+        uart_receive(echo_buffer, 1);
+        echo_buffer[0] = '\0';
+    }
+
+    return 0;
+}
---
+++ tests/test_uart.h
@@ -0,0 +1,6 @@
+#ifndef TEST_UART_H
+#define TEST_UART_H
+
+void test_uart(void);
+
+#endif
