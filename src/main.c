98561c9730c741218996e83d8e521732ccedc794
jairnarvaez <jairandresnar@gmail.com>
2026-02-03 22:51:48 -0500

    src/main: update main function for lsm303 test

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
 #include "test_buttons.h"
 #include "test_delay.h"
+#include "test_lsm303.h"
 #include "test_uart.h"
 
-#define SELECTED_TEST 2
+#define SELECTED_TEST 4
 
 #define TEST_UART 1
 #define TEST_BUTTONS 2
 #define TEST_DELAY 3
+#define TEST_LSM303 4
 
 int main(void)
 {
@@ -19,6 +21,9 @@ int main(void)
 #elif SELECTED_TEST == TEST_DELAY
     uint32_t time_us = 1000000;
     test_delay(time_us);
+
+#elif SELECTED_TEST == TEST_LSM303
+    test_lsm303();
 
 #else
 #error "No test selected or invalid test number"
