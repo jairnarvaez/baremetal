4ea9dd6938466440b3172a1e52c42a3cc0aece96
jairnarvaez <jairandresnar@gmail.com>
2026-02-05 19:35:10 -0500

    src/main: add cycle test switching using retained register

--- src/main.c
+++ src/main.c
@@ -1,35 +1,54 @@
+#include "power.h"
 #include "test_buttons.h"
 #include "test_delay.h"
 #include "test_lsm303.h"
 #include "test_uart.h"
 
-#define SELECTED_TEST 4
+#define TOTAL_TESTS 4
+#define DEFAULT_TEST 1
 
-#define TEST_UART 1
-#define TEST_BUTTONS 2
-#define TEST_DELAY 3
-#define TEST_LSM303 4
+/* GPREGRET: General Purpose Register Retained - survives soft resets
+Used to track which test was last executed and cycle through tests on each
+reset Address 0x4000051C */
+
+static uint32_t get_next_test_id(void)
+{
+    uint32_t test_id = POWER.GPREGRET;
+
+    if (test_id < 1 || test_id > TOTAL_TESTS) {
+        test_id = DEFAULT_TEST;
+    } else {
+        test_id = (test_id % TOTAL_TESTS) + 1;
+    }
+
+    POWER.GPREGRET = test_id;
+
+    return test_id;
+}
 
 int main(void)
 {
-#if SELECTED_TEST == TEST_UART
-    test_uart();
+    uint32_t current_test = get_next_test_id();
 
-#elif SELECTED_TEST == TEST_BUTTONS
-    test_buttons();
-
-#elif SELECTED_TEST == TEST_DELAY
-    uint32_t time_us = 1000000;
-    test_delay(time_us);
-
-#elif SELECTED_TEST == TEST_LSM303
-    test_lsm303();
-
-#else
-#error "No test selected or invalid test number"
-#endif
+    switch (current_test) {
+    case 1:
+        test_uart();
+        break;
+    case 2:
+        test_buttons();
+        break;
+    case 3:
+        test_delay(1000000);
+        break;
+    case 4:
+        test_lsm303();
+        break;
+    default:
+        break;
+    }
 
     for (;;) {
+        __asm("WFE");
     }
 
     return 0;
