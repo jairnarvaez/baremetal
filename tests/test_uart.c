e6c98dc6a7af169f7a2c5d3d80378accfa5c1e8e
jairnarvaez <jairandresnar@gmail.com>
2026-02-18 23:41:11 -0500

    tests/test_uart: re-enqueue rx in callback instead of busy loop

--- tests/test_uart.c
+++ tests/test_uart.c
@@ -7,12 +7,12 @@ char echo_buffer[ECHO_BUFFER_SIZE] = { 0 };
 void on_receive_msg(size_t bytes)
 {
     uart_send(echo_buffer);
+    uart_receive(echo_buffer, 1);
 }
 
 int test_uart(void)
 {
     uart_init(UART_BAUDRATE_115200);
-
     uart_send("\r\n");
     uart_send("================================\r\n");
     uart_send("   micro:bit v2 - UART Test    \r\n");
@@ -20,10 +20,6 @@ int test_uart(void)
 
     uart_send("Waiting for data...\r\n\r\n");
 
-    while (1) {
-        uart_receive(echo_buffer, 1);
-        echo_buffer[0] = '\0';
-    }
-
-    return 0;
+    uart_receive(echo_buffer, 1);
+    while (1) { }
 }
